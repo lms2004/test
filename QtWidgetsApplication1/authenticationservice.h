488da7bc70268743eb84ab990c95a20048133d85
LN <2146289682@qq.com>
2024-05-18 22:40:59 +0800

    1

--- QtWidgetsApplication1/authenticationservice.h
+++ QtWidgetsApplication1/authenticationservice.h
@@ -1,1 +1,24 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef AUTHENTICATIONSERVICE_H
+#define AUTHENTICATIONSERVICE_H
+
+#include <iostream>
+#include <unordered_map>
+#include <mysql.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
+
+class AuthenticationService {
+public:
+    AuthenticationService();
+    ~AuthenticationService();
+    bool registerUser(const std::string& username, const std::string& password);
+    bool login(const std::string& username, const std::string& password);
+    std::string hashPassword(const std::string& password);
+
+private:
+    std::unordered_map<std::string, std::string> users;
+    const unsigned char salt[50] = "Gk4^r8@qF0o!Z3Vb2y10$V5F9zgYjZLZ1%sZ3Zq5u8Nw8e";
+};
+
+#endif // AUTHENTICATIONSERVICE_H
--- QtWidgetsApplication1/register.h
+++ QtWidgetsApplication1/register.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include <QMainWindow>
+#include "ui_registerUser.h"
+#include "authenticationservice.h"
+
+class RegisterWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    RegisterWindow(QWidget* parent = nullptr);
+    ~RegisterWindow();
+
+private slots:
+    void on_pushButtonRegister_clicked();
+
+private:
+    Ui::RegisterWindow* ui;
+    QString username;
+    QString password;
+    QString performRegistration(const QString& username, const QString& password);
+};
+
+#endif // REGISTER_H
