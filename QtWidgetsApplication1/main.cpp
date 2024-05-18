3d623f56cbb46f1fc4d3a436ddb6e8fb2630e1d5
LN <2146289682@qq.com>
2024-05-18 18:41:05 +0800

    1

--- QtWidgetsApplication1/main.cpp
+++ QtWidgetsApplication1/main.cpp
@@ -1,6 +1,10 @@
 #include "Authentication.h"
 #include "login.h"
 #include <QApplication>
+#include <mysql.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
 
 //×¢²á/µÇÂ¼½çÃæ
 int main(int argc, char* argv[])
