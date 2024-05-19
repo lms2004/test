555cc9fc648abf711aa71a97a5b4f37ae24a5772
Myles266 <143015850+lms2004@users.noreply.github.com>
2024-05-19 23:44:10 +0800

    1

--- QtWidgetsApplication1/main.cpp
+++ QtWidgetsApplication1/main.cpp
@@ -2,13 +2,19 @@
 #include <QWidget>
 #include "AuthenticationService.h"
 #include "login.h"
-
+#include "register.h"
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
 
+
     LoginWindow window;
     window.show();
+
+
+    RegisterWindow window;
+    window.show();
+
 
     return a.exec();
 }
