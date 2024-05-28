a30fb323b71bc836ed6008fa5710a157e1528649
Myles266 <143015850+lms2004@users.noreply.github.com>
2024-05-28 23:31:28 +0800

    1

--- QtWidgetsApplication1/main.cpp
+++ QtWidgetsApplication1/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
 
     QObject::connect(&registerWindow, &RegisterWindow::registerSuccessful, [&]() {
         registerWindow.close();
-        uploaderWindow.show();
+        loginWindow.show();
         });
 
     QObject::connect(&registerWindow, &RegisterWindow::switchToLogin, [&]() {
